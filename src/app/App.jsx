bf395ede009a61eb268045464f8db93b69103e15
richardhillman <richard.cc.hillman@gmail.com>
2021-05-16 21:32:28 -0700

    minor pathing

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -6,8 +6,8 @@ import {
 } from 'react-router-dom';
 import LandingPage from '../pages/LandingPage';
 import ContactMePage from '../pages/ContactMePage';
-import listOfPartnersPage from '../pages/ListOfPartnersPage';
-import listOfBenefitsPage from '../pages/ListOfBenefitsPage';
+import ListOfPartnersPage from '../pages/ListOfPartnersPage';
+import ListOfBenefitsPage from '../pages/ListOfBenefitsPage';
 import AboutMePage from '../pages/AboutMePage';
 
 export default function App() {
@@ -19,8 +19,8 @@ export default function App() {
 
         <Route exact path="/" component={ LandingPage } />
         <Route exact path="/contact" component={ ContactMePage } />
-        <Route exact path="/partners" component={ listOfPartnersPage } />
-        <Route exact path="/benefits" component={ listOfBenefitsPage } />
+        <Route exact path="/partners" component={ ListOfPartnersPage } />
+        <Route exact path="/benefits" component={ ListOfBenefitsPage } />
         <Route exact path="/about" component={ AboutMePage } />
 
         </Switch>
